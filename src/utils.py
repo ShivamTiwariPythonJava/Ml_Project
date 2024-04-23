2f21e488fcdb5bc2e1e820548f44f8947ac4282f
ShivamTiwariPythonJava <ssttiwari6@gmail.com>
2024-04-22 23:02:19 -0400

    Hpyperparameter tunning

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ from sklearn.metrics import r2_score
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent))
 from exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -22,14 +23,26 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
+        # You create a GridSearchCV object with your model here in models her, the parameter grid (param_grid), and the number of folds for cross-validation (cv=3).
+        # You fit the GridSearchCV object to your training data (X_train, y_train).
+        # You extract the best parameters found by GridSearchCV.
+        # You update your model with the best parameters using set_params.
+        # You fit the updated model to your training data.
+        # Finally, you can use the updated model for making predictions.
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
 
-            model.fit(X_train, y_train) # Train model
+            gs = GridSearchCV(model,para, cv=3)
+
+            gs.fit(X_train, y_train) # Train model
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
 
