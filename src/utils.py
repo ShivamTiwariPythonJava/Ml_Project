e455200ac9d2d017732677eca1cde65f1fc8d2ba
ShivamTiwariPythonJava <ssttiwari6@gmail.com>
2024-04-25 23:17:47 -0400

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -22,40 +22,41 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-
-def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
-        # You create a GridSearchCV object with your model here in models her, the parameter grid (param_grid), and the number of folds for cross-validation (cv=3).
-        # You fit the GridSearchCV object to your training data (X_train, y_train).
-        # You extract the best parameters found by GridSearchCV.
-        # You update your model with the best parameters using set_params.
-        # You fit the updated model to your training data.
-        # Finally, you can use the updated model for making predictions.
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            para = param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model,para, cv=3)
-
-            gs.fit(X_train, y_train) # Train model
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
 
-            train_model_score = r2_score(y_train,y_train_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
 
-            test_model_score = r2_score(y_test,y_test_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
 
         return report
-    
-    except Exception as e:
-        raise CustomException(e,sys)
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
